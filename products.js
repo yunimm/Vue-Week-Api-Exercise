f4cda055069030510d985c5d0a6a1c2649c987b8
yumimm <yuu9916@gmail.com>
2022-01-21 02:31:00 +0800

    feat: 使用者若無登入直接進入商品頁面，會被導回登入頁面 使用者可以查看產品列表 使用者可以點擊單一產品，查看詳細資訊

---
+++ products.js
@@ -0,0 +1,50 @@
+        // 建立Vue環境
+const app = {
+  data() {
+    return {
+      apiUrl: 'https://vue3-course-api.hexschool.io/v2',
+      apiPath: 'yu-hexschool',
+      products: [],
+      tempProduct: {},
+    }
+  },
+  methods: {    
+    // 確認使用者身份，身份正確則登入，身份錯誤則導回登入頁面
+    checkAdmin() {
+      const url = `${this.apiUrl}/api/user/check`
+      axios.post(url)
+      .then(() => {
+        this.getData()
+      })
+      .catch((err) => {
+        alert(err.data.message)
+        window.location = 'index.html'
+      })
+    },
+    getData() {
+      const url = `${this.apiUrl}/api/${this.apiPath}/admin/products`
+      axios.get(url)
+      .then((res) => {
+        this.products = res.data.products
+      })
+      .catch((err) => {
+        alert(err.data.message)
+      })
+    },
+    openProduct(item) {
+      this.tempProduct = item
+    }
+  },
+ 
+  mounted() {
+    // 讀取token
+    const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*\=\s*([^;]*).*$)|^.*$/, "$1")
+    // 將token寫入axios預設設定
+    axios.defaults.headers.common.Authorization = token
+
+    this.checkAdmin()
+  }
+}
+Vue.createApp(app).mount('#app')
+
+
